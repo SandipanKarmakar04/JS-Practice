20d0225a6d61337fa50f256829c3dd5b3bb1265a
SandipanKarmakar04 <sandipan.sk7@gmail.com>
2023-11-16 13:43:52 +0530

    adding detain in array file

--- 02_Basics/01_array.js
+++ 02_Basics/01_array.js
@@ -1,1 +1,22 @@
 //ARRAY
+
+const myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+//console.log(myArray);
+//Shallow Copies
+//Deep Copies
+
+const myHeros = ["Ironman", "Captain America", "Black Panther"];
+//console.log(myHeros);
+
+const myArray2 = new Array(1, 2, 3, 4, 5, 6, 7, 8, 9);
+//myArray2.push(10);
+//myArray2.pop();
+//myArray2.unshift(0);
+//myArray2.shift();
+
+//console.log(myArray2.includes(5));
+//console.log(myArray2.indexOf(6));
+//const newArray = myArray2.join()
+
+//console.log(myArray2);
+//console.log(newArray);
