3d48fc7e03ff344f3b404686c6c0043618ea8784
SandipanKarmakar04 <sandipan.sk7@gmail.com>
2023-11-16 19:51:49 +0530

    add new file

--- 02_Basics/01_array.js
+++ 02_Basics/01_array.js
@@ -1,6 +1,6 @@
 //ARRAY
 
-const myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const myArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 //console.log(myArray);
 //Shallow Copies
 //Deep Copies
@@ -8,7 +8,7 @@ const myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 const myHeros = ["Ironman", "Captain America", "Black Panther"];
 //console.log(myHeros);
 
-const myArray2 = new Array(1, 2, 3, 4, 5, 6, 7, 8, 9);
+const myArray2 = new Array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
 //myArray2.push(10);
 //myArray2.pop();
 //myArray2.unshift(0);
@@ -20,3 +20,17 @@ const myArray2 = new Array(1, 2, 3, 4, 5, 6, 7, 8, 9);
 
 //console.log(myArray2);
 //console.log(newArray);
+
+/* ++++++++++ SLICE ++++++++++ */
+//Doesn't manipulate the original values.
+console.log("A. ", myArray);
+const s1 = myArray.slice(1, 3);
+console.log("Slice: ", s1);
+console.log(myArray);
+
+/* ++++++++++ SLICE ++++++++++ */
+//Manipulate the original values. 
+console.log("B. ",myArray);
+const s2 = myArray.splice(1, 3);
+console.log("Splice:", s2);
+console.log(myArray);
---
+++ 02_Basics/02_array.js
@@ -0,0 +1,27 @@
+const marvelHeros = ["Ironman", "Captain America", "Black Panther", "Spiderman"];
+const dclHeros = ["Superman", "Batman", "FLash"];
+
+/* ++++++++++ PUSH ++++++++++ */
+//This method add a new element in an array.
+//marvelHeros.push(dclHeros);
+//console.log(marvelHeros);
+
+/* ++++++++++ CONCAT ++++++++++ */
+//This method merge two arrays and create a new array.
+//const newHeros = marvelHeros.concat(dclHeros);
+//console.log(newHeros);
+
+/* ++++++++++ SPREAD ++++++++++ */
+//This method spread all the values of and array and break them into pieces.
+//const allNewHeros = [...marvelHeros, ...dclHeros];
+//console.log(allNewHeros);
+
+/* ++++++++++ FLAT ++++++++++ */
+//const myArray3 = [1, 2, 3, [4, 5, 6], 7, [8, 9, [10, 11]]]
+//const flatArray = myArray3.flat(Infinity);
+//console.log(flatArray);
+
+/* ++++++++++ FROM ++++++++++ */
+//Convert any strings to an array.
+console.log(Array.from("SANDIPAN"));
+
